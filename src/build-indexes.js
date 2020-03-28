481fec947aaf22983279729060f856d53c297aa8
unknown <rfainarea@gmail.com>
2020-03-28 22:11:17 +0200

    direct and indirect indexing

---
+++ src/build-indexes.js
@@ -0,0 +1,62 @@
+const fs = require("fs");
+const path = require("path");
+const cheerio = require("cheerio");
+
+const getDocumentText = require("./get-document-text");
+const getTextDictionary = require("./get-text-dictionary");
+
+function buildDirectIndex(inputPath) {
+  const queue = [""];
+  const docs = [];
+
+  function readDir() {
+    queue.forEach(queuedPath => {
+      fs.readdirSync(path.join(inputPath, queuedPath)).forEach(item => {
+        const relPath = path.join(queuedPath, item);
+        const absPath = path.join(inputPath, relPath);
+        const itemStats = fs.statSync(absPath);
+
+        if (itemStats.isDirectory()) {
+          return queue.push(relPath);
+        }
+
+        const htmlAsString = fs.readFileSync(absPath);
+        const $doc = cheerio.load(htmlAsString);
+        const documentText = getDocumentText($doc);
+        const textDictionary = getTextDictionary(documentText);
+
+        docs.push({ doc: relPath.replace(/\\/g, "/"), terms: textDictionary });
+      });
+    });
+  }
+
+  while (queue.length > 0) {
+    readDir();
+    queue.shift();
+  }
+
+  return docs;
+}
+
+function buildIndirectIndex(dirIndexDocs) {
+  const docsObj = {};
+
+  dirIndexDocs.forEach(d => {
+    Object.keys(d.terms).forEach(t => {
+      if (docsObj[t] === undefined) docsObj[t] = {};
+      docsObj[t][d.doc] = d.terms[t];
+    });
+  });
+
+  return Object.keys(docsObj).map(key => ({ term: key, docs: docsObj[key] }));
+}
+
+const buildIndexes = (inputPath, directIndexPath, indirectIndexPath) => {
+  const dirIndexDocs = buildDirectIndex(inputPath);
+  fs.writeFileSync(directIndexPath, JSON.stringify(dirIndexDocs, null, 4));
+
+  const indIndexDocs = buildIndirectIndex(dirIndexDocs);
+  fs.writeFileSync(indirectIndexPath, JSON.stringify(indIndexDocs, null, 4));
+};
+
+module.exports = buildIndexes;
---
+++ src/get-text-dictionary.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+var snowball = require("node-snowball");
+
+const stopWordsJson = fs.readFileSync("stop-words.json");
+const stopWords = JSON.parse(stopWordsJson);
+
+const getTextDictionary = docText => {
+  return docText
+    .match(/[\wăâîșț]+/gi)
+    .map(w => w.toLowerCase())
+    .map(w => snowball.stemword(w, "romanian"))
+    .filter(w => !stopWords.includes(w))
+    .reduce((acc, w) => {
+      acc[w] = acc[w] === undefined ? 1 : acc[w] + 1;
+      return acc;
+    }, {});
+};
+
+module.exports = getTextDictionary;
